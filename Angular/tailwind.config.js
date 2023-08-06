1fc25878ae30fdd02130f6b098f5a6916757e03a
Mia Coupland <miacoupland@btinternet.com>
2023-08-06 18:07:44 +0100

    Add tailwind

---
+++ Angular/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      purple: '#581c87',
+      bubblegum: '#ff77e9',
+      green: '#16a34a',
+      white: '#f8fafc'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
